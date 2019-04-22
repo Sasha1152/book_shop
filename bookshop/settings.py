7af8f084a243d481fe6edbe171ee14a26a3847eb
Sasha1152 <saxin@ukr.net>
2019-04-22 18:36:02 +0300

    creeate write object for images in the database

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -127,7 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
-
-MEDIA_URL = '/static/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+#
+MEDIA_URL = '/images/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
--- bookshop/urls.py
+++ bookshop/urls.py
@@ -32,5 +32,7 @@ urlpatterns = [
     path('user/', include('user.urls')),
     path('image/', include('image.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
