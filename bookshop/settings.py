9b09b46111810d7ffa5f2eb44b5893c6d94db844
Sasha1152 <saxin@ukr.net>
2019-04-22 16:01:43 +0300

    work with images

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -127,10 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'image/images')
-MEDIA_URL = '/image/images/'
+MEDIA_URL = '/static/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
--- bookshop/urls.py
+++ bookshop/urls.py
@@ -31,4 +31,6 @@ urlpatterns = [
     path('suborder/', include('suborder.urls')),
     path('user/', include('user.urls')),
     path('image/', include('image.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
